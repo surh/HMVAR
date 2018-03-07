c58dfb0d0684b71aa463ca1801d45888f0333138
surh <sur00mx@gmail.com>
2018-03-07 14:37:20 -0800

    Preparing to run on all files

--- inst/scripts/hmp16s_2.hmqcp_prevalence.r
+++ inst/scripts/hmp16s_2.hmqcp_prevalence.r
@@ -10,43 +10,43 @@ files <- data.frame(Name = c("hmqcp.v13.otu",
                     stringsAsFactors = FALSE)
 files
 
-
-
-i <- 1
-Dat <- format_input_hmqcp(name = files$Name[i],
-                          counts_file = files$counts[i],
-                          map_file = files$map[i],
-                          collapse_level = 6)
-
-# Calculate prevalence and change names to homgenize with HMMCP
-prev <- calculate_prevalence(Dat = Dat, thres = 1, group = "HMPbodysubsite")
-levels(prev$Group) <- c("Buccal mucosa", "Stool", "Supragingival plaque", "Tongue dorsum")
-# head(prev)
-
-# Sort
-prev <- prev[ order(prev$Group, prev$Proportion , decreasing = TRUE), ]
-prev$Taxon <- factor(prev$Taxon, unique(prev$Taxon))
-
-# Plot
-p1 <- ggplot(prev, aes(x = Taxon, y = Proportion, group = Group, col = Group )) +
-  facet_wrap(~ Group, ncol = 1) +
-  geom_line() +
-  theme(axis.text.x = element_blank()) +
-  theme_blackbox
-p1
-
-
-filename <- paste(files$Name[i], ".prevalence_by_site.svg", sep = "")
-cat(filename, "\n")
-ggsave(filename, p1, width = 4, height = 8)
-
-filename <- paste(files$Name[i], ".topprev.txt", sep = "")
-cat(filename, "\n")
-write.table(subset(prev, Proportion >= 0.75), file = filename,
-            sep = "\t", quote = FALSE, col.names = TRUE, row.names = FALSE)
-
-filename <- paste(files$Name[i], ".fullprev.txt", sep = "")
-cat(filename, "\n")
-write.table(prev, file = filename,
-            sep = "\t", quote = FALSE, col.names = TRUE, row.names = FALSE)
+for(i in 1:nrow(files)){
+  Dat <- format_input_hmqcp(name = files$Name[i],
+                            counts_file = files$counts[i],
+                            map_file = files$map[i],
+                            collapse_level = 6)
+  
+  # Calculate prevalence and change names to homgenize with HMMCP
+  prev <- calculate_prevalence(Dat = Dat, thres = 1, group = "HMPbodysubsite")
+  levels(prev$Group) <- c("Buccal mucosa", "Stool", "Supragingival plaque", "Tongue dorsum")
+  # head(prev)
+  
+  # Sort
+  prev <- prev[ order(prev$Group, prev$Proportion , decreasing = TRUE), ]
+  prev$Taxon <- factor(prev$Taxon, unique(prev$Taxon))
+  
+  # Plot
+  p1 <- ggplot(prev, aes(x = Taxon, y = Proportion, group = Group, col = Group )) +
+    facet_wrap(~ Group, ncol = 1) +
+    geom_line() +
+    theme(axis.text.x = element_blank()) +
+    theme_blackbox
+  p1
+  
+  
+  filename <- paste(files$Name[i], ".prevalence_by_site.svg", sep = "")
+  cat(filename, "\n")
+  ggsave(filename, p1, width = 4, height = 8)
+  
+  filename <- paste(files$Name[i], ".topprev.txt", sep = "")
+  cat(filename, "\n")
+  write.table(subset(prev, Proportion >= 0.75), file = filename,
+              sep = "\t", quote = FALSE, col.names = TRUE, row.names = FALSE)
+  
+  filename <- paste(files$Name[i], ".fullprev.txt", sep = "")
+  cat(filename, "\n")
+  write.table(prev, file = filename,
+              sep = "\t", quote = FALSE, col.names = TRUE, row.names = FALSE)
+  
+}
 
