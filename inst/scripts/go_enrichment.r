265325953c5fa532e503d56ab099afaaaf9d5149
surh <sur00mx@gmail.com>
2018-04-16 17:05:05 -0700

    adding enrichment by species and comparison

--- inst/scripts/go_enrichment.r
+++ inst/scripts/go_enrichment.r
@@ -41,7 +41,7 @@ for(c in comparisons){
   for(i in 1:length(sig_counts)){
     # i <- 1
     a <- names(sig_counts)[i]
-    cat("\t", a, "\n")
+    # cat("\t", a, "\n")
   
     q <- sig_counts[i]
     m <- bg_counts[a]
@@ -57,3 +57,61 @@ for(c in comparisons){
 }
 write.table(RES, "GO_enrichment_by_comparsions.txt", sep = "\t", quote = FALSE,
             col.names = TRUE, row.names = FALSE)
+
+Sys.time()
+
+##### REPEAT by genome and comparison ###########
+
+comparisons <- levels(interaction(Tab$A, Tab$B, Tab$Species, sep = "_", drop = TRUE))
+
+RES <- NULL
+for(c in comparisons){
+  # c <- comparisons[1]
+  cat(c, "\n")
+  specs <- paste(strsplit(c, split = "_")[[1]][-(1:2)], collapse = "_")
+  
+  # Get data from comparison
+  dat <- droplevels(subset(Tab, A == strsplit(x = c, split = "_")[[1]][1] & B == strsplit(x = c, split = "_")[[1]][2] & Species == specs))
+  # specs <- levels(dat$Species)
+  
+  ANNOTS <- NULL
+  for(s in specs){
+    # s <- specs[1]
+    cat("\t", s, "\n")
+    
+    go_file <- paste(go.dir, "/", s, ".GO.txt", sep = "")
+    cat("\t", go_file, "\n")
+    
+    go <- read.table(go_file, header = TRUE)
+    
+    ANNOTS <- rbind(ANNOTS, go)
+    rm(go)
+  }
+  
+  bg_counts <- table(ANNOTS$Annotation)
+  
+  sig_annots <- droplevels(ANNOTS[ ANNOTS$Gene %in% levels(dat$gene), ])
+  sig_counts <- table(sig_annots$Annotation)
+  
+  for(i in 1:length(sig_counts)){
+    # i <- 1
+    a <- names(sig_counts)[i]
+    # cat("\t", a, "\n")
+    
+    q <- sig_counts[i]
+    m <- bg_counts[a]
+    n <- nrow(ANNOTS) - m
+    k <- nrow(sig_annots)
+    
+    pval <- 1 - phyper(q = q - 1, m = m, n = n, k = k)
+    
+    res <- data.frame(comparison = c, annotation = a, nsig = q, nbg = m, pval = pval,
+                      row.names = NULL)
+    RES <- rbind(RES, res)
+  }
+}
+write.table(RES, "GO_enrichment_by_comparison_and_species.txt", sep = "\t", quote = FALSE,
+            col.names = TRUE, row.names = FALSE)
+
+
+
