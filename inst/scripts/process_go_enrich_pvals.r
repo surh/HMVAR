b921de99b665d83de7adfbdb0980aed9da44d328
surh <sur00mx@gmail.com>
2018-04-16 18:25:56 -0700

    Analysing by comparison and species

--- inst/scripts/process_go_enrich_pvals.r
+++ inst/scripts/process_go_enrich_pvals.r
@@ -2,6 +2,8 @@ library(HMAR)
 library(GO.db)
 library(qvalue)
 
+Sys.time()
+
 setwd("~/micropopgen/exp/2018/today/go_enrichment/")
 
 Tab <- read.table("GO_enrichment_by_comparsions.txt", sep = "\t", header = TRUE)
@@ -37,8 +39,48 @@ for(i in 1:nrow(Tab)){
   
   
 }
-write.table("GO_enrichment_by_comparsions_processed.txt")
+write.table(Tab, "GO_enrichment_by_comparsions_processed.txt")
 
 
 
-###########
+########### Repeat for species and comparisons ###########
+
+Tab <- read.table("GO_enrichment_by_comparison_and_species.txt", sep = "\t", header = TRUE)
+head(Tab)
+
+Tab$qval <- NULL
+for(c in levels(Tab$comparison)){
+  cat(c, "\n")
+  Tab$qval[ Tab$comparison == c ] <- p.adjust(Tab$pval[ Tab$comparison == c ], 'fdr')
+  # summary(qvalue::qvalue(Tab$pval[ Tab$comparison == c ], lambda = seq(0.01, 0.95, 0.05)))
+}
+p1 <- ggplot(Tab, aes(x = pval)) +
+  facet_wrap(~ comparison, ncol = 2) +
+  geom_histogram(bins = 20) +
+  AMOR::theme_blackbox
+p1
+ggsave("pvalues_go_enrich_by_comparison_and_species.svg", p1, width = 8, height = 20)
+
+head(Tab)
+
+Tab$GO_term <- NA
+Tab$GO_desc <- NA
+Tab$GO_ontology <- NA
+for(i in 1:nrow(Tab)){
+  # i <- 1
+  
+  go <- GO.db::GOTERM[[as.character(Tab$annotation[i])]]
+  if(!is.null(go)){
+    Tab$GO_term[i] <- go@Term
+    Tab$GO_desc[i] <- go@Definition
+    Tab$GO_ontology[i] <- go@Ontology
+  }
+  
+  
+}
+write.table(Tab, "GO_enrichment_by_comparisons_and_species_processed.txt")
+
+
+
+
+
