ac6b74c7549cba5e9b6b27e4f9b27749f2c87116
surh <sur00mx@gmail.com>
2018-12-19 12:12:27 -0800

    Fixing bug when freq_thres != 0.5

--- inst/bin/processing/combined_mktest.r
+++ inst/bin/processing/combined_mktest.r
@@ -39,13 +39,86 @@ genes <- c("638301.3.peg.1",
 # Parameters
 midas_dir <- "/godot/shared_data/metagenomes/hmp/midas/merge/2018-02-07.merge.snps.d.5/Granulicatella_adiacens_61980/"
 depth_thres <- 1
-map_file <- "../2018-12-14.hmp_mktest/hmp_SPvsTD_map.txt"
+map_file <- "/godot/users/sur/exp/fraserv/2018/2018-12-14.hmp_mktest/hmp_SPvsTD_map.txt"
+freq_thres <- 0.5
 
 
 mktest <- midas_mktest(midas_dir = midas_dir,
                        map_file = map_file,
                        genes = genes,
-                       depth_thres = depth_thres)
+                       depth_thres = depth_thres,
+                       freq_thres = 0.5)
+mktest
+
+######### Placing function code ############
+map <- readr::read_tsv(map_file)
+map <- map %>% dplyr::select(sample = ID, Group)
+Dat <- read_midas_data(midas_dir = midas_dir, map = map, 
+                       genes = genes)
+Dat$info <- determine_snp_effect(Dat$info)
+# Works up to here
+
+# Dat$info <- determine_snp_dist(info = Dat$info, freq = Dat$freq, 
+#                                depth = Dat$depth, map = map,
+#                                depth_thres = depth_thres, 
+#                                freq_thres = freq_thres)
+
+
+info <- Dat$info
+freq <- Dat$freq
+depth <- Dat$depth
+
+
+freq_thres <- 0.2
+
+if (freq_thres < 0 || freq_thres > 1) 
+  stop("ERROR: freq_thres must have values in [0, 1]", 
+       call. = TRUE)
+freq_thres <- min(freq_thres, 1 - freq_thres)
+depth <- depth %>% tidyr::gather(key = "sample", value = "depth", 
+                                 -site_id)
+freq <- freq %>% tidyr::gather(key = "sample", value = "freq", 
+                               -site_id)
+
+dat <- depth %>%
+  dplyr::inner_join(freq, by = c("site_id", "sample")) %>% 
+  dplyr::left_join(map, by = "sample") %>% 
+  dplyr::filter(depth >= depth_thres)
+# dat
+
+dat <- dat %>%
+  dplyr::mutate(allele = replace(freq, freq < freq_thres, "major")) %>%
+  dplyr::mutate(allele = replace(allele, freq >= (1 - freq_thres), "minor"))
+# dat
+
+dat <- dat %>%
+  dplyr::mutate(allele = replace(allele, (freq >= freq_thres) & (freq < (1 - freq_thres)), NA))
+dat
+
+  dplyr::filter(!is.na(allele))
+dat
+
+site_dist <- dat %>% split(.$site_id) %>% purrr::map_chr(get_site_dist)
+site_dist <- tibble::tibble(site_id = names(site_dist), distribution = factor(site_dist, 
+                                                                              levels = c("Fixed", "Invariant", "Polymorphic")))
+info <- info %>% dplyr::inner_join(site_dist, by = "site_id")
+return(info)
+
+
+
+
+
+
+
+
+Res <- Dat$info %>% split(.$gene_id) %>% purrr::map_dfr(mkvalues, 
+                                                        depth_thres = depth_thres, .id = "gene_id")
+return(Res)
+
+
+
+
+
 
 vmwa <- read_tsv("hmp.vmwa.pvals.genes/Granulicatella_adiacens_61980_vmwa.genes.txt")
 vmwa <- vmwa %>% filter(gene_id %in% genes)
