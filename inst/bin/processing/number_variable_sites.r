8df5d1d90e6cea87ba9b8261a9acfd99a0b14272
surh <sur00mx@gmail.com>
2018-12-21 12:07:33 -0800

    Turn into executable script

--- inst/bin/processing/number_variable_sites.r
+++ inst/bin/processing/number_variable_sites.r
@@ -21,18 +21,88 @@ library(HMVAR)
 library(tidyverse)
 library(argparser)
 
-# setwd("/godot/users/sur/exp/fraserv/2018/today3/")
+################ FUNCTIONS ################
+process_arguments <- function(){
+  p <- arg_parser(paste0("Calculates and plots the number of variable sites ",
+                         "per sample within a genome (or a subset of genes in that ",
+                         "genome."))
+  
+  p <- add_argument(p, "midas_dir",
+                    help = paste0("Directory with output from merging MIDAS SNPs. ",
+                                  "Must contain 'snps_info.txt', 'snps_depth.txt' ",
+                                  "and 'snps_freq.txt'."),
+                    type = "character")
+  p <- add_argument(p, "map_file",
+                    help = paste0("Mapping file associating samples with groups. ",
+                                  "Must have 'Group' and 'ID' columns."),
+                    type = "character")
+  p <- add_argument(p, "--genes",
+                    help = paste0("File with subset of genes to analyze. ",
+                                  "It must be one gene ID per line without headers."),
+                    type = "character",
+                    default = NULL)
+  p <- add_argument(p, "--cds_only",
+                    help = paste0("Flag indicating whether only sites at CDSs should ",
+                                  "be considered."),
+                    flag = TRUE)
+  p <- add_argument(p, "--depth_thres",
+                    help = paste0("Minumum number of reads at a given site ",
+                                  "at a given sample, for that site in that ",
+                                  "sample to be included in calculations"),
+                    default = 1,
+                    type = "integer")
+  p <- add_argument(p, "--freq_thres",
+                    help = paste0("This value is the threshold that will be ",
+                                  "used to determine which allele is present in ",
+                                  "a sample. The value from ",
+                                  "min(freq_thres, 1 - freq_thres) represents the ",
+                                  "distance from 0 or 1, for a site to be assigned ",
+                                  "to the major or minor allele respectively. ",
+                                  "It must be a value in [0,1]."),
+                    default = 0.5,
+                    type = "double")
+  p <- add_argument(p, "--outdir",
+                    help = paste0("Directory to write (and create if needed) to write ",
+                                  "the output."),
+                    default = "results/",
+                    type = "character")
+  p <- add_argument(p, "--plot_position",
+                    help = paste0("Flag indicating whether to plot the proportion ",
+                                  "of variable sites by genomic position. Might be ",
+                                  "time consuming."),
+                    flag = TRUE)
+  p <- add_argument(p, "--prefix",
+                    help = paste0("Prefix for output filenames."),
+                    default = "out",
+                    type = "character")
+
+  # Read arguments
+  args <- parse_args(p)
+  
+  # Process arguments
+  if(args$freq_thres < 0 || args$freq_thres > 1)
+    stop("ERROR: freq_thres must be a value in [0,1].")
+  if(args$depth_thres < 0)
+    stop("ERROR: depth_thres must be a non-negative integer.")
+  if(!dir.exists(args$midas_dir))
+    stop("ERROR: midas_dir does not exist.")
+  if(!file.exists(args$map_file))
+    stop("ERROR: map_file does not exist.")
+  
+  return(args)
+}
 
 # Parameters
-args <- list(midas_dir = "/godot/users/sur/exp/fraserv/2018/2018-12-14.hmp_mktest/genomes/Granulicatella_adiacens_61980/",
-             map_file = "/godot/users/sur/exp/fraserv/2018/2018-12-14.hmp_mktest/hmp_SPvsTD_map.txt",
-             genes = NA,
-             depth_thres = 1,
-             freq_thres = 0.5,
-             cds_only = FALSE,
-             plot_position = TRUE,
-             prefix = "Granulicatella_adiacens_61980",
-             outdir = "results/")
+# args <- list(midas_dir = "/godot/users/sur/exp/fraserv/2018/2018-12-14.hmp_mktest/genomes/Granulicatella_adiacens_61980/",
+#              map_file = "/godot/users/sur/exp/fraserv/2018/2018-12-14.hmp_mktest/hmp_SPvsTD_map.txt",
+#              genes = NA,
+#              depth_thres = 1,
+#              freq_thres = 0.5,
+#              cds_only = FALSE,
+#              plot_position = TRUE,
+#              prefix = "Granulicatella_adiacens_61980",
+#              outdir = "results/")
+args <- process_arguments()
 
 # Read genes
 if(is.na(args$genes)){
