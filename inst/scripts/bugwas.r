5b076e26ecc43386395f7c58cd3a107520feb75f
surh <sur00mx@gmail.com>
2019-01-24 16:41:02 -0800

    bimbam gneotype and phenotype files

--- inst/scripts/bugwas.r
+++ inst/scripts/bugwas.r
@@ -16,3 +16,41 @@ Dat <- read_midas_data(midas_dir = args$midas_dir,
                        map = map,
                        genes = NULL)
 
+
+
+# Match freqs and depth
+Dat$depth <- Dat$depth %>% gather(key = "sample", value = 'depth', -site_id)
+Dat$freq <- Dat$freq %>% gather(key = "sample", value = 'freq', -site_id)
+Dat$info <- Dat$info %>% select(site_id, ref_id, major_allele, minor_allele)
+
+# Find missing data
+dat <- Dat$depth %>%
+  inner_join(Dat$freq, by = c("site_id", "sample"))
+dat$freq[ dat$depth < 1 ] <- NA
+Dat$freq <- dat %>% select(-depth) %>% spread(sample, freq)
+
+geno <- Dat$info %>%
+  select(site_id, minor_allele, major_allele) %>%
+  left_join(Dat$freq, by = "site_id")  
+
+pheno <- map %>%
+  filter(sample %in% colnames(geno)) %>%
+  arrange(factor(sample, levels = colnames(geno)[-(1:3)])) %>%
+  mutate(phenotype = 1*(Group == "Supragingival.plaque")) %>%
+  select(id = sample, phenotype)
+
+
+
+
+%>%
+  left_join(map, by = "sample") %>%
+  filter(depth >= depth_thres) %>%
+  left_join(meta, by = "site_id") %>%
+  mutate(allele = replace(freq, freq < 0.5, 'major')) %>%
+  mutate(allele = replace(allele, freq >= 0.5, 'minor')) %>%
+  filter(distribution != "Invariant")
+dat
+
+dat <- dat %>%
+  mutate(site_id = factor(site_id,
+                          levels = as.character(unique(sort(as.numeric(dat$site_id))))))
