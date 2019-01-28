04deb40d98e33c84059032b6d429355899ce982e
surh <sur00mx@gmail.com>
2019-01-28 11:13:11 -0800

    Cleaning code to run LMM association via GEMMA

--- inst/scripts/bugwas.r
+++ inst/scripts/bugwas.r
@@ -15,8 +15,7 @@ args <- list(midas_dir = "/godot/shared_data/metagenomes/hmp/midas/merge/2018-02
              prefix = "Actinomyces_odontolyticus_57475",
              gemma = "~/bin/gemma.0.93b",
              bimbam = "~/bin/bimbam",
-             loglik_null_line = 17,
-             PC_branch_cors = FALSE)
+             gemma_version = 'bugwas')
 
 # test_genes <- c("411466.7.peg.516", "411466.7.peg.602",
 #                 "411466.7.peg.603", "411466.7.peg.604",
@@ -38,12 +37,12 @@ map <- read_tsv(args$map_file, col_types = 'cc')
 map <- map %>% select(sample = ID, Group = Group)
 
 # Convert to bimbam
-bimbam_dir <- file.path(args$outdir, "bimbam")
-Files$Dirs$bimbam_dir <- bimbam_dir
-midas_bimbam <- midas_to_bimbam(midas_dir = args$midas_dir, map = map, outdir = bimbam_dir, prefix = NULL)
+Files$Dirs$bimbam_dir <- file.path(args$outdir, "bimbam")
+midas_bimbam <- midas_to_bimbam(midas_dir = args$midas_dir, map = map, outdir = Files$Dirs$bimbam_dir, prefix = NULL)
 Files$Files$midas_geno_file <- midas_bimbam$filenames$geno_file
 Files$Files$pheno_file <- midas_bimbam$filenames$pheno_file
 Files$Files$snp_file <- midas_bimbam$filenames$snp_file
+rm(map)
 
 # Run bimbam
 cmd <- paste(args$bimbam,
@@ -60,19 +59,18 @@ cmd <- paste(args$bimbam,
 cat("Running\n\t>", cmd, "\n")
 out <- system(cmd)
 # Re-organize files
-imputed_dir <- file.path(args$outdir, "imputed")
-dir.create(imputed_dir)
-Files$Dirs$imputed_dir <- imputed_dir
-file.copy("output/imputed.log.txt", imputed_dir)
-file.copy("output/imputed.mean.genotype.txt", imputed_dir)
-file.copy("output/imputed.snpinfo.txt", imputed_dir)
+Files$Dirs$imputed_dir <- file.path(args$outdir, "imputed")
+dir.create(Files$Dirs$imputed_dir)
+file.copy("output/imputed.log.txt", Files$Dirs$imputed_dir)
+file.copy("output/imputed.mean.genotype.txt", Files$Dirs$imputed_dir)
+file.copy("output/imputed.snpinfo.txt", Files$Dirs$imputed_dir)
 file.remove("output/imputed.log.txt")
 file.remove("output/imputed.mean.genotype.txt")
 file.remove("output/imputed.snpinfo.txt")
-Files$Files$imputed_geno_file <- file.path(imputed_dir, "imputed.mean.genotype.txt")
+Files$Files$imputed_geno_file <- file.path(Files$Dirs$imputed_dir, "imputed.mean.genotype.txt")
 
 # Prepare data for gemma
-Dat_gemma <- list(geno = read_table2(file.path(imputed_dir, "imputed.mean.genotype.txt"),
+Dat_gemma <- list(geno = read_table2(file.path(Files$Dirs$imputed_dir, "imputed.mean.genotype.txt"),
                                      col_names = colnames(midas_bimbam$Dat$geno),
                                      col_types = paste0('c', 'c', 'c',
                                                         paste(rep('n', ncol(midas_bimbam$Dat$geno) - 3),
@@ -105,13 +103,12 @@ file.remove("output/kinship.log.txt")
 file.remove("output/kinship.cXX.txt")
 Files$Files$kinship_file <- file.path(Files$Dirs$kinship_dir, "kinship.cXX.txt")
 
-
-# SVD & PCA
-# Since there are no patterns all genotypes have the same weighth
-geno <- Dat_gemma$geno %>% select(-site_id, -minor_allele, -major_allele) %>%
-  as.matrix %>% t
-geno.svd <- svd(geno)
-geno.pca <- prcomp(geno)
+# # SVD & PCA
+# # Since there are no patterns all genotypes have the same weighth
+# geno <- Dat_gemma$geno %>% select(-site_id, -minor_allele, -major_allele) %>%
+#   as.matrix %>% t
+# geno.svd <- svd(geno)
+# geno.pca <- prcomp(geno)
 
 # Run lmm
 cmd <- paste(args$gemma,
@@ -134,86 +131,86 @@ file.remove("output/lmm.assoc.txt")
 Files$Files$lmm_assoc_file <- file.path(Files$Dirs$lmm_dir, "lmm.assoc.txt")
 Files$Files$lmm_log_file <- file.path(Files$Dirs$lmm_dir, "lmm.log.txt")
 
-# Process lmm results
-# Get lognull and lambda
-# For newer GEMMA versions I need vg/ve from two diff lines.
-lognull <- scan(Files$Files$lmm_log_file,
-                what = character(0),
-                sep = "\n")[args$loglik_null_line] %>%
-  strsplit(" ") %>%
-  unlist %>%
-  last
-lambda <- scan(Files$Files$lmm_log_file,
-                what = character(0),
-                sep = "\n")[13] %>%
-  strsplit(" ") %>%
-  unlist %>%
-  last
-
-# # Read results
-# lmm_res <- read_tsv(Files$Files$lmm_assoc_file,
-#                     col_types = cols(rs = 'c')) %>% 
-#   select(chr, rs, ps, lambda_H1 = l_mle,
-#          loglik_H1 = logl_H1, p.value = p_lrt) %>%
-#   mutate(minus.log.p = -log10(p.value))
+# # Process lmm results
+# # Get lognull and lambda
+# # For newer GEMMA versions I need vg/ve from two diff lines.
+# lognull <- scan(Files$Files$lmm_log_file,
+#                 what = character(0),
+#                 sep = "\n")[args$loglik_null_line] %>%
+#   strsplit(" ") %>%
+#   unlist %>%
+#   last
+# lambda <- scan(Files$Files$lmm_log_file,
+#                 what = character(0),
+#                 sep = "\n")[13] %>%
+#   strsplit(" ") %>%
+#   unlist %>%
+#   last
 # 
-# lmm <- list(lmm = lmm_res,
-#             lognull = as.numeric(lognull),
-#             lambda = as.numeric(lambda))
-lmm_res <- read_tsv(Files$Files$lmm_assoc_file,
-                    col_types = cols(rs = 'c')) %>%
-  mutate(negLog10 = -log10(p_lrt))
-
-lmm.bi <- list(lmm = lmm_res, "lognull" = as.numeric(lognull),
-     lambda = as.numeric(lambda))
-     
-cor.geno <- bugwas:::get_correlations(XX = geno,
-                                      pca = geno.pca$x,
-                                      npcs = length(Dat_gemma$pheno$id),
-                                      id = Dat_gemma$pheno$id)
-lmm <- list(logreg.bi = NULL, lmm.bi = lmm.bi,
-            lognull = lognull,
-            lambda = lambda, cor.XX = cor.geno)
-
-
-
-
-
-
-# Get a dendrogram of samples
-# TEMPORARY. MOVE EARLIER
-tre <- hclust(dist(dist(geno)))
-tre <- ape::as.phylo(tre)
-Files$Files$phylo_file <- file.path(args$outdir, "bimbam/phylo.tre")
-ape::write.tree(phy = tre, file = Files$Files$phylo_file)
-
-# Get list of all tree info
-treeInfo <- bugwas:::get_tree(phylo = Files$Files$phylo_file,
-                              prefix = 'actino',
-                              XX.ID = Dat_gemma$pheno$id,
-                              pca = geno.pca,
-                              npcs = length(Dat_gemma$pheno$id),
-                              allBranchAndPCCor = args$PC_branch_cors)
+# # # Read results
+# # lmm_res <- read_tsv(Files$Files$lmm_assoc_file,
+# #                     col_types = cols(rs = 'c')) %>% 
+# #   select(chr, rs, ps, lambda_H1 = l_mle,
+# #          loglik_H1 = logl_H1, p.value = p_lrt) %>%
+# #   mutate(minus.log.p = -log10(p.value))
+# # 
+# # lmm <- list(lmm = lmm_res,
+# #             lognull = as.numeric(lognull),
+# #             lambda = as.numeric(lambda))
+# lmm_res <- read_tsv(Files$Files$lmm_assoc_file,
+#                     col_types = cols(rs = 'c')) %>%
+#   mutate(negLog10 = -log10(p_lrt))
+# 
+# lmm.bi <- list(lmm = lmm_res, "lognull" = as.numeric(lognull),
+#      lambda = as.numeric(lambda))
+#      
+# cor.geno <- bugwas:::get_correlations(XX = geno,
+#                                       pca = geno.pca$x,
+#                                       npcs = length(Dat_gemma$pheno$id),
+#                                       id = Dat_gemma$pheno$id)
+# lmm <- list(logreg.bi = NULL, lmm.bi = lmm.bi,
+#             lognull = lognull,
+#             lambda = lambda, cor.XX = cor.geno)
 
 
 
-# Ridge regression
-wald <- bugwas:::wald_test(y = Dat_gemma$pheno$phenotype,
-                           XX = geno,
-                           svd.XX = geno.svd,
-                           lambda = lmm$lambda,
-                           XX.all = XX.all,
-                           prefix = 'actino',
-                           npcs = length(Dat_gemma$pheno$id),
-                           pca = geno.pca)
 
 
 
-ggplot(lmm_res, aes(x = ps, y = negLog10)) +
-  facet_grid(~chr, space = "free_x", scales = "free_x") +
-  geom_hline(yintercept = 8, color = "red", size = 3) +
-  geom_point()
-
-ggplot(lmm_res, aes(x = p_lrt)) +
-  geom_histogram(bins = 20) +
-  AMOR::theme_blackbox()
+# # Get a dendrogram of samples
+# # TEMPORARY. MOVE EARLIER
+# tre <- hclust(dist(dist(geno)))
+# tre <- ape::as.phylo(tre)
+# Files$Files$phylo_file <- file.path(args$outdir, "bimbam/phylo.tre")
+# ape::write.tree(phy = tre, file = Files$Files$phylo_file)
+# 
+# # Get list of all tree info
+# treeInfo <- bugwas:::get_tree(phylo = Files$Files$phylo_file,
+#                               prefix = 'actino',
+#                               XX.ID = Dat_gemma$pheno$id,
+#                               pca = geno.pca,
+#                               npcs = length(Dat_gemma$pheno$id),
+#                               allBranchAndPCCor = args$PC_branch_cors)
+# 
+# 
+# 
+# # Ridge regression
+# wald <- bugwas:::wald_test(y = Dat_gemma$pheno$phenotype,
+#                            XX = geno,
+#                            svd.XX = geno.svd,
+#                            lambda = lmm$lambda,
+#                            XX.all = XX.all,
+#                            prefix = 'actino',
+#                            npcs = length(Dat_gemma$pheno$id),
+#                            pca = geno.pca)
+# 
+# 
+# 
+# ggplot(lmm_res, aes(x = ps, y = negLog10)) +
+#   facet_grid(~chr, space = "free_x", scales = "free_x") +
+#   geom_hline(yintercept = 8, color = "red", size = 3) +
+#   geom_point()
+# 
+# ggplot(lmm_res, aes(x = p_lrt)) +
+#   geom_histogram(bins = 20) +
+#   AMOR::theme_blackbox()
