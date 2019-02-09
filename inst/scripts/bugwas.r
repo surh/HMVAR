3a954e06a3bc5b178006f5285a581fb56a015c20
surh <sur00mx@gmail.com>
2019-02-08 19:33:15 -0800

    Run lmm with gemma function

--- inst/scripts/bugwas.r
+++ inst/scripts/bugwas.r
@@ -92,7 +92,7 @@ bimbam_impute <- function(geno_file, pheno_file, pos_file,
 #' @export
 gemma_kinship <- function(geno_file, pheno_file, snp_file,
                           gemma = 'gemma',
-                          outdir = "./",
+                          outdir = "kinship/",
                           prefix = "kinship"){
   
   cmd <- paste(gemma,
@@ -116,6 +116,58 @@ gemma_kinship <- function(geno_file, pheno_file, snp_file,
   kinship_file <- file.path(outdir, paste(c(prefix, "cXX.txt"), collapse = "."))
   
   return(kinship_file)
+}
+
+#' Run LMM via GEMMA
+#' 
+#' @param geno_file Mean genotype file in BIMBAM format. See GEMMA
+#' manual for details.
+#' @param pheno_file Phenotype file in BIMBAM format. See GEMMA
+#' manual for details.
+#' @param snp_file SNP annotation file in BIMBAM format. See GEMMA
+#' manual for details.
+#' @param kinship_file Kinship file in GEMMA format. See GEMMA
+#' manual for details.
+#' @param gemma GEMMA executable.
+#' @param outdir directory to write output.
+#' @param maf Minor allele frequency threshold for SNPs to test.
+#' @param prefix Prefix for output filenames.
+#' 
+#' @references 
+#' http://www.xzlab.org/software/GEMMAmanual.pdf
+#' 
+#' @export
+gemma_lmm <- function(geno_file, pheno_file, snp_file, kinship_file,
+                      gemma = "gemma",
+                      outdir = "lmm/",
+                      maf = 0,
+                      prefix = "lmm"){
+  
+  # Run lmm
+  cmd <- paste(gemma,
+               "-g", geno_file,
+               "-p", pheno_file,
+               "-a", snp_file,
+               "-k", kinship_file,
+               "-lmm", 2,
+               "-o", prefix,
+               "-maf", maf)
+  out <- run_command(cmd)
+  
+  # Re-organize files
+  dir.create(outdir)
+  
+  filename <- paste0("output/", paste(c(prefix, "log.txt"), collapse = "."))
+  file.copy(filename, outdir)
+  file.remove(filename)
+  lmm_log_file <- file.path(outdir, paste(c(prefix, "log.txt"), collapse = "."))
+  
+  filename <- paste0("output/", paste(c(prefix, "assoc.txt"), collapse = "."))
+  file.copy(filename, outdir)
+  file.remove(filename)
+  lmm_assoc_file <- file.path(outdir, paste(c(prefix, "assoc.txt"), collapse = "."))
+  
+  return(c(lmm_log_file, lmm_assoc_file))
 }
 
 # The following will be based on bugwas:
@@ -162,7 +214,10 @@ map <- map %>% select(sample = ID, Group = Group)
 
 # Convert to bimbam
 Files$Dirs$bimbam_dir <- file.path(args$outdir, "bimbam")
-midas_bimbam <- midas_to_bimbam(midas_dir = args$midas_dir, map = map, outdir = Files$Dirs$bimbam_dir, prefix = NULL)
+midas_bimbam <- midas_to_bimbam(midas_dir = args$midas_dir,
+                                map = map,
+                                outdir = Files$Dirs$bimbam_dir,
+                                prefix = NULL)
 Files$Files$midas_geno_file <- midas_bimbam$filenames$geno_file
 Files$Files$pheno_file <- midas_bimbam$filenames$pheno_file
 Files$Files$snp_file <- midas_bimbam$filenames$snp_file
@@ -191,6 +246,21 @@ Files$Files$kinship_file <- gemma_kinship(geno_file = Files$Files$imputed_geno_f
                                           outdir = Files$Dirs$kinship_dir,
                                           prefix = 'kinship')
 
+# Run lmm
+Files$Dirs$lmm_dir <- file.path(args$outdir, "lmm")
+res <- gemma_lmm(geno_file = Files$Files$imputed_geno_file,
+          pheno_file = Files$Files$pheno_file,
+          snp_file = Files$Files$snp_file,
+          kinship_file = Files$Files$kinship_file,
+          gemma = args$gemma,
+          outdir = Files$Dirs$lmm_dir,
+          maf = 0,
+          prefix = "lmm")
+Files$Files$lmm_log_file <- res[1]
+Files$Files$lmm_assoc_file <- res[2]
+rm(res)
+
+
 # Prepare data for gemma
 Dat_gemma <- list(geno = read_table2(file.path(Files$Dirs$imputed_dir, "imputed.mean.genotype.txt"),
                                      col_names = colnames(midas_bimbam$Dat$geno),
@@ -200,20 +270,9 @@ Dat_gemma <- list(geno = read_table2(file.path(Files$Dirs$imputed_dir, "imputed.
                     arrange(factor(site_id, levels = midas_bimbam$Dat$snp$ID)),
                   pheno = midas_bimbam$Dat$pheno,
                   snp = midas_bimbam$Dat$snp)
-
 # Cleanup
 rm(midas_bimbam, cmd, out)
 gc()
-
-
-
-# # SVD & PCA
-# # Since there are no patterns all genotypes have the same weighth
-geno <- Dat_gemma$geno %>% select(-site_id, -minor_allele, -major_allele) %>%
-  as.matrix %>% t
-geno.svd <- svd(geno)
-geno.pca <- prcomp(geno)
-rm(geno)
 
 # Run lmm
 cmd <- paste(args$gemma,
@@ -280,6 +339,13 @@ lambda <- scan(Files$Files$lmm_log_file,
 #             lambda = lambda, cor.XX = cor.geno)
 
 
+# # SVD & PCA
+# # Since there are no patterns all genotypes have the same weighth
+geno <- Dat_gemma$geno %>% select(-site_id, -minor_allele, -major_allele) %>%
+  as.matrix %>% t
+geno.svd <- svd(geno)
+geno.pca <- prcomp(geno)
+# rm(geno)
 
 # Wald test
 # wald <- wald_test(y = y,
