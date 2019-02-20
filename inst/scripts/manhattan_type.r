8647debc1e6d587e137a8d5bb0f427b7ba1a57bb
surh <sur00mx@gmail.com>
2019-02-20 00:09:25 -0800

    Finding file and priting plot to html

--- inst/scripts/manhattan_type.r
+++ inst/scripts/manhattan_type.r
@@ -9,7 +9,7 @@ for(lmm_file in files){
   # lmm_file <- "../today5/output/Actinomyces_odontolyticus_57475_lmm.results.txt"
   genome <- basename(lmm_file) %>% str_replace("_lmm.results.txt$", "")
   cat(genome, "\n")
-  lmm <- read_tsv(lmm_file,
+  lmm <- read_tsv(file.path(indir,lmm_file),
                   col_types = 'ccnnnnnnnc')
   # lmm
   
@@ -19,7 +19,7 @@ for(lmm_file in files){
     geom_point(aes(color = type)) +
     geom_hline(yintercept = -log10(1e-6), color = "red", size = 2) +
     theme_classic()
-  p1
+  print(p1)
   filename <- paste0(outdir, "/", genome, ".manhattan.png")
   ggsave(filename, p1, width = 16, height = 4)
 }
