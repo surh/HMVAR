e32246d6bdbf2bdb70be68a70bf19e4d805ba399
surh <sur00mx@gmail.com>
2019-03-15 12:11:34 -0700

    Setting bins

--- inst/scripts/mice_imputation_tests.r
+++ inst/scripts/mice_imputation_tests.r
@@ -15,11 +15,11 @@ map
 Dat <- midas_to_bimbam(midas_dir = "midas_output_small/", outdir = "bimbam", map = map, focal_group = "Supragingival.plaque", prefix = "test")
 
 
-myfun <- function(d, m = 5){
+myfun <- function(d, m = 5, verbose = FALSE){
   d %>%
     select(-site_id, -minor_allele, -major_allele) %>%
     filter_all(any_vars(!is.na(.))) %>%
-    mice(m = 5) %>% 
+    mice(m = 5, printFlag = verbose) %>% 
     mice::complete() %>%
     as_tibble
 }
@@ -28,30 +28,11 @@ imp <- Dat$Dat$geno %>% split(Dat$Dat$snp$chr) %>%
   map_dfr(~myfun(.), .id = "site_id", m1 = 1)
 imp
 
+table(is.na(Dat$Dat$geno[,-(1:3)]))
+table(is.na(imp[,-1]))
 
-i <- d[[5]] %>%
-  select(-site_id, -minor_allele, -major_allele) %>%
-  filter_all(any_vars(!is.na(.))) %>%
-  mice(m = 5) %>% 
-  mice::complete()
-
-table(is.na(d[[5]][,-(1:3)]))
-table(is.na(i))
-
-
-
-
-i[is.na(d[[5]][,-(1:3)]) & !is.na(i)]
-as.matrix(d[[5]][,-(1:3)])[is.na(d[[5]][,-(1:3)]) & !is.na(i)]
-
-
-
-rowSums(!is.na(d[[5]]))
-colSums(!is.na(d[[5]]))
-
-Dat$Dat$geno %>%
-  select(-site_id, -minor_allele, -major_allele) %>%
-  filter_all(any_vars(!is.na(.)))
-
-tibble(x = c(NA,1,NA), y = c(1,1,NA)) %>% filter_all(any_vars(!is.na(.)))
-tibble(x = c(0,1,0), y = c(1,1,0)) %>% filter_all(all_vars(!is.na(.)))
+summary(as.matrix(imp[,-1])[ is.na(Dat$Dat$geno[,-(1:3)]) & !is.na(imp[,-1]) ])
+as.matrix(imp[,-1])[ is.na(Dat$Dat$geno[,-(1:3)]) & !is.na(imp[,-1]) ] %>% tibble(x = .) %>%
+  ggplot(aes(x = x)) +
+  geom_histogram(bins = 20) +
+  AMOR::theme_blackbox()
