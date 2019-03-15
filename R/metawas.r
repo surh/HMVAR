a5d70793e538dbbbe53a463356465817081cd9f6
surh <sur00mx@gmail.com>
2019-03-15 16:56:07 -0700

    Moving functions to package

--- R/metawas.r
+++ R/metawas.r
@@ -161,3 +161,88 @@ gemma_lmm <- function(geno_file, pheno_file, snp_file, kinship_file,
   
   return(c(lmm_log_file, lmm_assoc_file))
 }
+
+#' Imputation via mice
+#' 
+#' Imputes missing genotypes using mice
+#'
+#' @param geno A data table with genotype information. First three columns
+#' must be site_id, minor_allele and major allele; folowed by one column per
+#' sample with the sample ID as column name. Missing genotypes must be encoded
+#' as NA values
+#' @param snp SNP information data table. Must have columns ID, pos and chr.
+#' ID mut correspond to the "site_id" column in \code{geno}, and the SNPs must
+#' be in the same order in both tables.
+#' @param outdir Output directory to create and write the imputed table in
+#' BIMBAM format.
+#' @param m Number of multiple imputations. See \link{mice} documentation.
+#' @param verbose Whether to print progress during imputation.
+#' @param prefix Prefix for imputed file.
+#' @param return_table If TRUE, the function will return a list with both
+#' the output file path and the imputed table. If FALSE, only the file path
+#' will be returned.
+#' @param seed Seed for imputation. See \link{mice} documentation.
+#'
+#' @return Either the output file path, or a list containing the file path
+#' and the imputed table.
+#' 
+#' @export
+#' 
+#' @importFrom magrittr %>%
+mice_impute <- function(geno, snp,
+                        outdir = "imputed/",
+                        m = 5,
+                        verbose = FALSE,
+                        prefix = "imputed",
+                        return_table = FALSE,
+                        seed = NA){
+  
+  if(any(geno$site_id != snp$ID)){
+    stop("ERROR: geno and snp tables do not match", call. = TRUE)
+  }
+  
+  imp <- geno %>%
+    split(snp$chr) %>%
+    purrr::map_dfr(~tidy_mice(.), m1 = m, verbose = verbose, seed = seed) %>%
+    dplyr::arrange(match(site_id, snp$ID))
+  
+  # Write results
+  dir.create(outdir)
+  
+  filename <- file.path(outdir,
+                        paste(c(prefix, "mean.genotype.txt"), collapse = "."))
+  readr::write_tsv(imp, path = filename, col_names = FALSE)
+  
+  if(return_table){
+    return(list(imputed_file = filename, imp = imp))
+  }else{
+    return(filename)
+  }
+}
+
+#' Tidy mice
+#' 
+#' Internal utility function
+#' 
+#' Calls mice on data table
+#'
+#' @param d 
+#' @param m 
+#' @param verbose 
+#'
+#' @return A tibble with imputed results
+#' 
+#' @importFrom maggritr %>%
+tidy_mice <- function(d, m = 5, verbose = FALSE, seed = NA){
+  res <- d %>%
+    dplyr::select(site_id, minor_allele, major_allele)
+  
+  d <- d %>%
+    dplyr::select(-site_id, -minor_allele, -major_allele) %>%
+    dplyr::filter_all(dplyr::any_vars(!is.na(.))) %>%
+    mice::mice(m = m, printFlag = verbose, seed = seed) %>% 
+    mice::complete() %>%
+    dplyr::as_tibble()
+  
+  res %>% dplyr::bind_cols(d)
+}
