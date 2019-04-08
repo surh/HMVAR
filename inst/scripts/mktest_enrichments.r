494f62ab72c2823976ab8336639c0a315823bbc7
surh <sur00mx@gmail.com>
2019-04-07 23:50:10 -0700

    preparing to run

--- inst/scripts/mktest_enrichments.r
+++ inst/scripts/mktest_enrichments.r
@@ -1,5 +1,86 @@
 library(tidyverse)
 
+process_annotation <- function(mkres, sig_genes,
+                               annotation = "GO_terms",
+                               count_thres = 3,
+                               match_go = TRUE){
+  annotation <- "KEGG_KOs"
+  
+  BG <- mkres %>%
+    select(gene_id, annot = annotation) %>%
+    pmap_dfr(expand_annot) %>%
+    mutate(sig_gene = gene_id %in% sig_genes) %>%
+    filter(!is.na(term))
+  
+  res <- test_annotation(BG = BG, count_thres = count_thres, match_go = match_go)
+  
+  return(res)
+}
+
+expand_annot <- function(gene_id, annot){
+  annot <- str_split(string = annot, pattern = ",") %>% unlist
+  tibble(gene_id = gene_id,
+         term = annot)
+}
+
+
+test_annotation <- function(BG, count_thres = 3, match_go = TRUE){
+  # Get list to test
+  to_test <- BG %>%
+    filter(sig_gene) %>%
+    count(term) %>%
+    filter(n >= count_thres) %>%
+    select(term, n.sig = n)
+  
+  # Get BG counts
+  bg_counts <- BG %>%
+    filter(term %in% to_test$term) %>%
+    count(term) %>%
+    select(term, n.bg = n)
+  
+  selection.size <- BG %>%
+    filter(sig_gene) %>%
+    select(gene_id) %>%
+    unique() %>%
+    nrow()
+  
+  bg.size <- BG$gene_id %>% unique %>% length
+  
+  # Test
+  test_res <- to_test %>%
+    left_join(bg_counts, by = "term") 
+  
+  if(nrow(test_res) == 0){
+    return(test_res)
+  }
+  
+  test_res <- test_res %>%
+    pmap_dfr(function(term, n.sig, n.bg, selection.size, bg.size){
+      mat <- matrix(c(n.sig, n.bg, selection.size, bg.size), ncol = 2)
+      res <- fisher.test(mat)
+      tibble(term = term,
+             n.sig = n.sig,
+             n.bg = n.bg,
+             OR = res$estimate,
+             p.value = res$p.value)
+    }, selection.size = selection.size, bg.size = bg.size) %>%
+    mutate(q.value = p.adjust(p.value, 'fdr')) %>%
+    arrange(q.value)
+  
+  if(match_go){
+    # Match metadata
+    test_res <- test_res %>%
+      bind_cols(test_res %>%
+                  select(term) %>%
+                  unlist %>%
+                  AnnotationDbi::select(GO.db::GO.db, .,
+                                        columns = c("ONTOLOGY","TERM","DEFINITION"))) %>%
+      arrange(p.value)
+  }
+  
+  return(test_res)
+}
+
 process_mkres <- function(spec, mktest, annots){
   # i <- 1
   # spec <- files$spec[i]
@@ -44,10 +125,12 @@ process_mkres <- function(spec, mktest, annots){
 }
 
 
-
 args <- list(mkdir = "../2019-04-02.hmp_mktest_data/Buccal.mucosa/results/",
              annotdir = "../2019-04-01.hmp_subsite_annotations/hmp.subsite_annotations/",
              outdir = "Buccal.mucosa/")
+args <- list(mkdir = opts[1],
+             annotdir = opts[2],
+             outdir = opts[3])
 
 # Get list of files
 mktest <- tibble(mktest = file.path(args$mkdir, list.files(args$mkdir)))
@@ -69,9 +152,13 @@ files <- mktest %>%
 rm(mktest, annots)
 files
 
+dir.create(args$outdir)
 
 mkres <- files %>%
   pmap_dfr(process_mkres)
+mkres$p.value[ mkres$p.value > 1 ] <- 1
+filename <- file.path(args$outdir, "mkres_combined.txt")
+write_tsv(mkres, filename)
 
 mkres %>% arrange(p.value)
 mkres %>% arrange(desc(Dn))
@@ -80,9 +167,43 @@ mkres %>% arrange(desc(mkratio))
 mkres %>% arrange(desc(kaks))
 
 p1 <- mkres %>%
-  ggplot(aes(x = mkratio, y = p.value)) +
-  geom_point(aes(size = Dn)) +
-  scale_y_log10() +
+  ggplot(aes(x = mkratio, y = -log10(p.value))) +
+  geom_point(aes(size = Dn), alpha = 0.5) + 
   scale_x_log10() +
   AMOR::theme_blackbox()
+p1
+filename <- file.path(args$outdir, "mkres_combined_volcano.png")
+ggsave(filename, p1, width = 6, height = 5, dpi = 200)
+
+sig_genes <- mkres %>% filter(OR > 1) %>%
+  arrange(p.value)
+sig_genes
+sig_genes <- sig_genes %>%
+  select(gene_id) %>%
+  unique() %>%
+  unlist
+
+go_res <- process_annotation(mkres = mkres,
+                             sig_genes = sig_genes,
+                             annotation = "GO_terms",
+                             count_thres = 3, match_go = TRUE)
+go_res
+filename <- file.path(args$outdir, "go_enrichments.txt")
+write_tsv(go_res, filename)
+
+ko_res <- process_annotation(mkres = mkres,
+                             sig_genes = sig_genes,
+                             annotation = "KEGG_KOs",
+                             count_thres = 3, match_go = FALSE)
+ko_res
+filename <- file.path(args$outdir, "ko_enrichments.txt")
+write_tsv(ko_res, filename)
+
+og_res <- process_annotation(mkres = mkres,
+                             sig_genes = sig_genes,
+                             annotation = "OGs",
+                             count_thres = 3, match_go = FALSE)
+og_res
+filename <- file.path(args$outdir, "og_enrichments.txt")
+write_tsv(og_res, filename)
 
