fd59379401be17d812b8ebdd790106d9cdacb069
surh <sur00mx@gmail.com>
2019-06-10 17:45:18 -0700

    Cleaning

--- inst/scripts/get_gene_alns.r
+++ inst/scripts/get_gene_alns.r
@@ -19,27 +19,11 @@ library(tidyverse)
 # library(seqinr)
 library(HMVAR)
 
-# setwd("~/micropopgen/exp/2019/today4/")
-# setwd("/godot/users/sur/exp/fraserv/2019/today5/")
-
 depth_thres <- 1
 freq_thres <- 0.5
 
 keep_last_codon <- TRUE
 outdir <- 'results/'
-
-# mkdir <- "../2019-04-02.hmp_mktest_data/Buccal.mucosa/results/"
-# genome_dir <- "~/micropopgen/data/genomes/midas_db_v1.2/hmp.subsite/"
-# closest_dir <- "../today3/hmp.subsite.closest/"
-
-# mkdir <- "../2019-04-02.hmp_subsite_mktest/Buccal.mucosa/results/"
-# genome_dir <- "~/micropopgen/data/midas_db_v1.2/rep_genomes/"
-# closest_dir <- "hmp.subsite.closest/"
-# map_file <- "../2019-04-02.hmp_subsite_mktest/Buccal.mucosa/map.txt"
-# midas_dir <- "/godot/shared_data/metagenomes/hmp/midas/merge/2018-02-07.merge.snps.d.5/"
-# group <- "Buccal.mucosa"
-# missing_as <- 'major'
-# filter_sample <- TRUE
 
 mkdir <- "mkres/"
 genome_dir <- "genomes/"
@@ -148,50 +132,3 @@ for(mkres_file in mkres_files){
   }
   
 }
-# 
-# map <- read_tsv('map.txt') %>%
-#   select(sample = ID, Group = Group)
-# map
-# Dat <- read_midas_data(midas_dir = "midas_output_small/", map = map)
-# 
-# 
-# genome_fasta <- seqinr::read.fasta('genome/genome.fna.gz')
-# genome_feats <- readr::read_tsv('genome/genome.features.gz',
-#                                 col_types = readr::cols(.default = readr::col_character(),
-#                                                         start = readr::col_integer(),
-#                                                         end = readr::col_integer()))
-# 
-# genes <- unique(Dat$info$gene_id)
-# # gene <- genes[1]
-# # gene
-# 
-# for(gene in genes){
-#   # Get ref sequence
-#   gene_pos <- genome_feats %>%
-#     filter(gene_id == gene) %>%
-#     select(start, scaffold_id, end, strand)
-#   if(nrow(gene_pos) != 1){
-#     stop("ERROR: no data on genes")
-#   }
-#   gene_seq <- get_sequence_fragment(genome_fasta,
-#                                     ref_id = gene_pos$scaffold_id,
-#                                     start = gene_pos$start,
-#                                     end = gene_pos$end)
-#   
-#   # Get snp data
-#   all_sites <- gene_midas_data(Dat = Dat, gene = gene,
-#                                depth_thres = depth_thres,
-#                                freq_thres = freq_thres)
-#   # Set relative positions
-#   all_sites <- all_sites %>%
-#     mutate(ref_pos = ref_pos - gene_pos$start + 1)
-#   
-#   aln <- gene_snv_aln(snps = all_sites, seq = gene_seq,
-#                       missing_as = missing_as,
-#                       strand = gene_pos$strand,
-#                       keep_last_codon = keep_last_codon)
-#   
-#   
-#   filename <- file.path(outdir, paste0(gene, '_aln.fasta'))
-#   seqinr::write.fasta(aln$seq, aln$nam, file.out = filename)
-# }
