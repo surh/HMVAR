3ec47f0a53bfee6409e838033ec70edeb0099c6d
surh <sur00mx@gmail.com>
2019-07-02 19:25:48 -0700

    printing backgrounds for gsea

--- inst/scripts/mktest_enrichments2.r
+++ inst/scripts/mktest_enrichments2.r
@@ -31,13 +31,13 @@ if(length(annot_files) < length(mkres_files)){
 
 Dat <- tibble()
 for(i in 1:length(mkres_files)){
-  # i <- 3
+  # i <- 5
   input <- mkres_files[i]
   prefix <- prefixes[i]
   annotations <- str_subset(annot_files, pattern = prefix)
-  input
-  prefix
-  annotations
+  # input
+  # prefix
+  # annotations
   
   if(length(annotations) != 1){
     cat("Input:", input, "\n")
@@ -65,9 +65,8 @@ for(i in 1:length(mkres_files)){
     mkres <- mkres %>%
       filter(Dn > 0 & Ds > 0)
   }
-  mkres
+  # mkres
   if(nrow(mkres) == 0) next
-  
   
   cat("\tReading annotations...\n")
   annots <- read_eggnog(file.path(annot_dir, annotations)) %>%
@@ -79,7 +78,7 @@ for(i in 1:length(mkres_files)){
                                  pattern = "\\([+-]\\)_[0-9]",
                                  replacement = ""))
   ##########
-  annots
+  # annots
   
   # Calculate score
   cat("Calculate signed -log10(p-value)...\n")
@@ -91,75 +90,36 @@ for(i in 1:length(mkres_files)){
   gw.test <- annots %>%
     right_join(mkres, by = "gene_id") %>%
     select(gene_id, terms, score) %>%
-    filter(!is.na(score))
-  gw.test
+    filter(!is.na(score)) %>%
+    filter(!is.na(terms))
+  # gw.test
   cat("\tGenes with annotation: ", sum(!is.na(gw.test$terms)), "\n")
   if(nrow(gw.test %>% filter(!is.na(terms)) %>% filter(score != 0)) > 0){
     cat("Testing\n")
-    res <- gsea(gw.test, test = 'wilcoxon', alternative = 'two.sided', min_size = 3)
-    filename <- paste0(str_replace(prefix, '^\\^', ''), ".enrichments.txt")
-    filename <- file.path(outdir, filename)
-    filename
-    write_tsv(res, path = filename)
+    res <- gsea(gw.test, test = 'wilcoxon', alternative = 'two.sided', min_size = 3) %>%
+      filter(!is.na(p.value))
+    
+    if(nrow(res) > 0){
+      filename <- paste0(str_replace(prefix, '^\\^', ''), ".enrichments.txt")
+      filename <- file.path(outdir, filename)
+      # filename
+      write_tsv(res, path = filename)
+    }
   }
   
   Dat <- Dat %>% bind_rows(gw.test)
 }
 
-
-
-term_gsea <- function(genes, scores, test = "wilcoxon",
-                      alternative = "greater", min_size = 3){
-  
-  if(!is.character(genes)){
-    stop("ERROR: genes must be a character vector", call. = TRUE)
-  }
-  if(!is.numeric(scores) || is.null(attr(scores, "names"))){
-    stop("ERROR: scores must be a named numeric vector", call. = TRUE)
-  }
-  
-  ii <- names(scores) %in% genes
-  
-  if(sum(ii) < min_size){
-    return(tibble::tibble(size = sum(ii),
-                          median = NA,
-                          mean = NA,
-                          bg.median = NA,
-                          bg.mean = NA,
-                          statistic = NA,
-                          p.value = NA))
-  }
-  if(sum(!ii) < 1){
-    return(tibble::tibble(size = sum(ii),
-                          median = NA,
-                          mean = NA,
-                          bg.median = NA,
-                          bg.mean = NA,
-                          statistic = NA,
-                          p.value = NA))
-  }
-  
-  if(test == 'wilcoxon'){
-    res <- wilcox.test(scores[ii], scores[!ii], alternative = alternative)
-  }else if(test == 'ks'){
-    res <- ks.test(scores[ii], scores[!ii], alternative = alternative)
-  }else{
-    stop("ERROR: Invalid test", call. = TRUE)
-  }
-  
-  tibble::tibble(size = sum(ii),
-                 median = median(scores[ii]),
-                 mean = mean(scores[ii]),
-                 bg.median = median(scores),
-                 bg.mean = mean(scores),
-                 statistic = res$statistic,
-                 p.value = res$p.value)
-}
+sign(res$mean - res$bg.mean)
+res
 
 
 Dat
 Dat %>% filter(!is.na(terms)) %>% filter(score != 0) %>% head(200)
 
-gsea(dat = Dat %>% filter(!is.na(terms)) %>% filter(score != 0) %>% head(200),
-     test = 'wilcoxon', alternative = 'two.sided', min_size = 3) %>%
+Res <- gsea(dat = Dat,
+            test = 'wilcoxon',
+            alternative = 'two.sided',
+            min_size = 3) %>%
   filter(!is.na(p.value))
+Res
