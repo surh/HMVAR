0e5b844d4f60dbe67e74c98ca6f7ec3924bf14f2
surh <sur00mx@gmail.com>
2019-09-25 17:35:38 -0700

    calculate_fst executable
    moving code to other scripts

--- inst/scripts/calculate_fst.r
+++ inst/scripts/calculate_fst.r
@@ -1,160 +1,127 @@
-library(HMVAR)
+#!/usr/bin/env Rscript
+
+# (C) Copyright 2019 Sur Herrera Paredes
+# 
+# This file is part of HMVAR.
+# 
+# HMVAR is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# HMVAR is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with HMVAR.  If not, see <http://www.gnu.org/licenses/>.
+
+# setwd("~/micropopgen/exp/2019/today")
+# devtools::document(pkg = "~/micropopgen/src/HMVAR/")
+
 library(tidyverse)
-library(poolfstat)
-setwd("~/micropopgen/exp/2019/today")
-devtools::document(pkg = "~/micropopgen/src/HMVAR/")
+library(HMVAR)
+library(argparser)
 
-map <- read_tsv("midas/map.txt")
-map <- map %>% select(sample=ID, Group)
+process_arguments <- function(){
+  p <- arg_parser(paste("Calculate Fst/Fst_pool for all sites in MIDAS output"))
+  
+  # Positional arguments
+  p <- add_argument(p, "midas_dir",
+                    help = paste("Directory with midas merge output. Must have",
+                                 "snps_info.txt, snps_freq.txt and snps_depth.txt.",
+                                 "Altenatively, a directory containing subdirectories",
+                                 "with midas merge output."),
+                    type = "character")
+  
+  # Optional arguments
+  p <- add_argument(p, "--type",
+                    help = paste("Indicates whether midas_dir is a single species ('single') or",
+                                 "a directory containing multiple species directories ('multi')"),
+                    type = "character",
+                    default = "single")
+  p <- add_argument(p, "--map_file",
+                    help = paste("Mapping file. Must be tab-delimited and have ",
+                                 "ID and Group columns. Only samples in the mapping file",
+                                 "will be considered."),
+                    type = "character",
+                    default = "map.txt")
+  p <-add_argument(p, "--method",
+                   help = paste("Either 'Fst' or 'Fstpool'"),
+                   default = "Fstpool",
+                   type = "character")
+  p <- add_argument(p, "--outdir",
+                    help = paste("Directory for output"),
+                    type = "character",
+                    default = "output/")
+  
+  # Read arguments
+  cat("Processing arguments...\n")
+  args <- parse_args(p)
+  
+  # Process arguments
+  # Check type
+  if(!(args$type %in% c('sinlge', 'multi')))
+    stop("ERROR: --type must be one of 'single' or 'multi'")
+  
+  # Check method
+  if(!(args$method %in% c('Fst', 'Fstpool'))){
+    stop("ERROR: --method must be one of 'Fst' or 'Fstpool'")
+  }else if(args$method == "Fst"){
+    args$method <- "Weir-Cockerham"
+  }else if(args$method == "Fstpool"){
+    args$method <- 'Fstpool'
+  }
+  
+  return(args)
+}
 
-Dat <- read_midas_data("midas/merged.snps/Veillonella_parvula_57794/", map = map, cds_only = FALSE)
-gc()
+# Read arguments
+args <- process_arguments()
 
-map <- map %>% filter(sample %in% colnames(Dat$freq))
-map$Group %>% table
+# Read map
+map <- read_tsv("midas/map.txt") %>% select(sample=ID, Group)
 
+# Get list of midas_dirs
+if(args$type == 'multi'){
+  args$midas_dir <- list.dirs(args$midas_dir, recursive = FALSE)
+}
 
-# i <- 1
-# freq <- Dat$freq[i,]
-# support <- Dat$depth[i,]
-# info <- Dat$info[i, ]
-# map <- map
-# support_thres <- 1
-# method = "Fst_pool"
-# 
-# dat <- match_freq_and_depth(freq = freq,
-#                             depth = support,
-#                             info = info,
-#                             map = map,
-#                             depth_thres = support_thres,
-#                             verbose = FALSE)
+if(!dir.exists(args$outdir)){
+  dir.create(args$outdir)
+}
 
-
-fst <- calculate_fst(Dat = list(info = Dat$info[1:10000, ],
-                                depth = Dat$depth[1:10000,],
-                                freq = Dat$freq[1:10000,]),
-                     method = "Weir-Cockerham",
-                     support_thres = 1,
-                     map = map,
-                     sorted = TRUE,
-                     verbose = TRUE)
-
-fst_pool <- calculate_fst(Dat = list(info = Dat$info[1:10000, ],
-                                     depth = Dat$depth[1:10000,],
-                                     freq = Dat$freq[1:10000,]),
-                          method = "Fstpool",
-                          support_thres = 1,
-                          map = map,
-                          sorted = TRUE,
-                          verbose = TRUE)
-
-
-
-readcoverage <- (Dat$depth[1:10000,] %>%
-                   select(-site_id) %>%
-                   as.matrix())
-refallele.readcount <- readcoverage - round((Dat$freq[1:10000,] %>%
-                                               select(-site_id) %>%
-                                               as.matrix()) * readcoverage)
-
-readcoverage
-refallele.readcount
-
-groups <- setNames(map$Group, nm = map$sample)
-groups <- groups[colnames(readcoverage)]
-readcoverage <- AMOR::pool_samples(readcoverage, groups)
-refallele.readcount <- AMOR::pool_samples(refallele.readcount, groups)
-
-Dat.pooldata <- new("pooldata",
-                    npools = 4,
-                    nsnp = 10000,
-                    refallele.readcount = refallele.readcount,
-                    readcoverage = readcoverage,
-                    snp.info = Dat$info[1:10000,] %>% select(ref_id, ref_pos, major_allele, minor_allele) %>% as.matrix(),
-                    poolsizes = table(groups) %>% as.vector(),
-                    poolnames = colnames(readcoverage))
-FSTpool <- computeFST(pooldata = Dat.pooldata, method = "Anova")
-FSTpool$snp.FST
-
-fst_pool$fst$FSTpool <- FSTpool$snp.FST
-fst_pool$fst$Fst <- fst$fst$Fst
-fst_pool$fst <- fst_pool$fst %>%
-  mutate(FSTpool = replace(FSTpool, FSTpool < 0, 0))
-fst_pool$fst
-
-p1 <- ggplot(fst_pool$fst, aes(x= Fst_pool, y = FSTpool)) +
-  geom_point() +
-  AMOR::theme_blackbox()
-p1
-
-p1 <- ggplot(fst_pool$fst, aes(x= Fst, y = Fst_pool)) +
-  geom_point() +
-  AMOR::theme_blackbox()
-p1
-
-pairs(fst_pool$fst[,9:11])
-
-fst_pool$fst %>%
-  gather(key = "Estimator", value = "Fst", Fst, Fst_pool, FSTpool) %>%
-  ggplot(aes(x = ref_pos, y = Fst)) +
-  facet_grid(Estimator ~ .) +
-  geom_point()
-
-p1 <- ggplot(fst_pool$fst, aes(x = ref_pos, y = Fst)) +
-  geom_point() +
-  AMOR::theme_blackbox()
-p1
-
-summary(fst_pool$fst$FSTpool)
-hist(log10(fst_pool$fst$FSTpool))
-
-# summary(fst$Fst)
-# hist(fst$Fst)
-# 
-# snp_effects <- determine_snp_effect(info = Dat$info %>%
-#                                       select(site_id, major_allele, minor_allele, amino_acids) %>%
-#                                       filter(!is.na(amino_acids)))
-w_fst <- window_fst(dat = fst$fst, w_size = 1000, s_size = 300, sorted = TRUE)
-w_fst_pool <- window_fst(dat = fst_pool$fst, w_size = 1000, s_size = 300, sorted = TRUE)
-
-w_fst
-w_fst_pool 
-
-
-w_fst_pool %>%
-  arrange(desc(Fst_pool))
-
-
-
-p1 <- ggplot(w_fst, aes(x = (start + end) / 2, y = Fst)) +
-  facet_grid(~ ref_id, scales = "free_x", space = "free_x") +
-  # geom_point(aes(size = n_sites)) +
-  geom_point() +
-  geom_hline(yintercept = 0.25, color = "red", size = 2) +
-  ggplot2::theme(panel.background = ggplot2::element_blank(), 
-                 panel.grid = ggplot2::element_blank(),
-                 axis.text = ggplot2::element_text(color = "black"), 
-                 axis.title = ggplot2::element_text(color = "black", face = "bold"), 
-                 axis.line.x.bottom = ggplot2::element_line(),
-                 axis.line.y.left = ggplot2::element_line())
-p1
-
-p1 <- ggplot(w_fst_pool, aes(x = (start + end) / 2, y = Fst_pool)) +
-  facet_grid(~ ref_id, scales = "free_x", space = "free_x") +
-  # geom_point(aes(size = n_sites)) +
-  geom_point() +
-  geom_hline(yintercept = 0.25, color = "red", size = 2) +
-  ggplot2::theme(panel.background = ggplot2::element_blank(), 
-                 panel.grid = ggplot2::element_blank(),
-                 axis.text = ggplot2::element_text(color = "black"), 
-                 axis.title = ggplot2::element_text(color = "black", face = "bold"), 
-                 axis.line.x.bottom = ggplot2::element_line(),
-                 axis.line.y.left = ggplot2::element_line())
-p1
-
-p1 <- ggplot(w_fst, aes(x = Fst, y = n_sites)) +
-  geom_point() +
-  geom_smooth(method = "loess") +
-  AMOR::theme_blackbox() 
-p1
-
+# Calculate for each midas dir
+args$midas_dir %>%
+  purrr::map(function(midas_dir, method,
+                      support_thres = 1,
+                      map, sorted, outdir){
+    spec <- basename(midas_dir)
+    cat(spec, "\n")
+    
+    # Read data
+    Dat <- read_midas_data(midas_dir = midas_dir, map = map, cds_only = FALSE)
+    Dat <- list(info = Dat$info[1:1000, ],
+                depth = Dat$depth[1:1000,],
+                freq = Dat$freq[1:1000,])
+    
+    # map <- map %>% filter(sample %in% colnames(Dat$freq))
+    # map$Group %>% table
+    
+    # Calculate Fst
+    fst <- calculate_fst(Dat = Dat,
+                         method = method,
+                         support_thres = support_thres,
+                         map = map %>% filter(sample %in% colnames(Dat$freq)),
+                         sorted = sorted,
+                         verbose = TRUE)
+    
+    filename <- file.path(outdir, paste0(spec, ".fst.txt"))
+    readr::write_tsv(fst$fst, filename)
+    
+  }, method = args$method,
+  support_thres = 1,
+  map = map,
+  sorted = args$sorted,
+  outdir = args$outdir)
